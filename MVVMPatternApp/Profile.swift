06e4857ca544e2b04274f58c5b8c08d00fb6d1cc
Yurij Alt <yura_chekalyuk@mail.ru>
2022-01-16 00:03:56 +0300

    MVC Project

---
+++ MVVMPatternApp/Profile.swift
@@ -0,0 +1,14 @@
+//
+//  Profile.swift
+//  MVVMPatternApp
+//
+//  Created by Юрий Чекалюк on 15.01.2022.
+//
+
+import Foundation
+
+struct Profile {
+    var name: String
+    var secondName: String
+    var age: Int
+}
--- MVVMPatternApp/ViewController.swift
+++ MVVMPatternApp/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    //MARK: - IBOutlets
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var secondNameLabel: UILabel!
+    @IBOutlet weak var ageLabel: UILabel!
+    
+    var profile: Profile? {
+        didSet {
+            guard let profile = profile else { return }
+            
+            self.nameLabel.text = profile.name
+            self.secondNameLabel.text = profile.secondName
+            self.ageLabel.text = "\(profile.age)"
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-       
+        profile = Profile(name: "Юрий", secondName: "Чекалюк", age: 32)
     }
-
-
+    
 }
 
