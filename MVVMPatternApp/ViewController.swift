e50171fe7e073149ca727eadef73cde44b432118
Yurij Alt <yura_chekalyuk@mail.ru>
2022-01-15 23:37:06 +0300

    Initial Commit

---
+++ MVVMPatternApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMPatternApp
+//
+//  Created by Юрий Чекалюк on 15.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
